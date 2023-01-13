228e2cc64c7ced8c0c23e9e068c5f3938d1945ba
Hana <122632599+ThePalmeraTech@users.noreply.github.com>
2023-01-13 13:01:47 -0500

    Add files via upload

---
+++ pan-territory-data.js
@@ -0,0 +1,143 @@
+
+  
+    var provinciaObject = {
+      "Bocas del Toro": {
+        "Almirante": ["Almirante", "Barriada Guaymí", "Barrio Francés", "Nance de Riscó", "Valle de Aguas Arriba", "Valle de Riscó"],
+        "Bocas del Toro": ["Bastimentos", "Bocas del Toro", "Cauchero", "Punta Laurel", "Tierra Oscura"],
+        "Changuinola": ["Changuinola", "Cochigro", "El Empalme", "Guabito", "La Gloria", "Las Delicias", "Las Tablas", "Barriada 4 de Abril", "El Teribe", "Finca 30", "Finca 60", "Finca 6", "El Silencio"],
+        "Chiriquí Grande": ["Bajo Cedro", "Chiriquí Grande", "Miramar", "Punta Peña", "Punta Robalo", "Rambala"]    
+      },
+      "Coclé": {
+        "Aguadulce": ["Aguadulce", "Barrios Unidos", "El Cristo", "El Roble", "El Hato de San Juan de Dios", "Pocrí", "Pueblos Unidos", "Virgen del Carmen"],
+        "Antón": ["Antón", "Cabellero", "Cabuya", "El Chirú", "El Retiro", "El Valle", "Juan Díaz",  "Río Hato", "San Juan de Dios", "Santa Rita"],
+        "La Pintada": ["El Harino", "El Potrero", "La Pintada", "Las Lomas", "Llano Grande", "Piedras Gordas", "Llano Norte"],
+        "Natá": ["Capellanía", "El Caño", "Guzmán", "Las Huacas", "Natá", "Toza", "Villarreal"],
+        "Olá": ["El Copé", "El Palmar", "El Picacho", "La Pava", "Olá"],
+        "Penonomé": ["Cañaveral", "Coclé", "Chiriguí Arriba", "El Coco", "Pajonal", "Penonomé", "Río Grande", "Río Indio", "Toabré", "Tulú"]
+      },
+      "Colón": {
+        "Colón": ["Barrio Norte", "Barrio Sur", "Buena Vista", "Cativá", "Ciricito", "Cristóbal", "Cristóbal Este", "Escobal", "Limón", "Nueva Providencia", "Puerto Pilón", "Sabanitas", "Salamanca", "San Juan", "Santa Rosa"],
+        "Chagres": ["Achiote", "El Guabo", "La Encantada", "Nuevo Chagres", "Palmas Bellas", "Piña", "Salud"],
+        "Donoso": ["Coclé del Norte", "El Guásimo", "Gobea", "Miguel de la Borda", "Rio Indio"],
+        "Portobelo": ["Cacique", "Garrote", "Isla Grande", "María Chiquita", "Portobelo"],
+        "Santa Isabel": ["Cuango", "Miramar", "Nombre de Dios", "Palenque", "Palmira", "Playa Chiquita", "Santa Isabel", "Viento Frío"],
+        "Omar Torrijos Herrera": ["San José del General", "San Juan de Turbe", "Nueva Esperanza"]    
+      },
+      "Chirquí": {
+        "Alanje": ["Alanje", "Divalá", "Canta Gallo", "El Tejar", "Guarumal", "Nuevo México", "Querévalo", "Palo Grande", "Santo Tomás"],
+        "Barú": ["Baco", "Limones", "Progreso", "Puerto Armuelles", "Rodolfo Aguilar Delgado"],
+        "Boquerón": ["Bágala", "Boquerón", "Cordillera", "Guabal", "Guayabal", "Paraíso", "Pedregal", "Tijeras"],
+        "Boquete": ["Alto Boquete", "Bajo Boquete", "Caldera", "Jaramillo", "Los Naranjos", "Palmira"],
+        "Bugaba": ["Aserrío de Gariché", "Bugaba", "Eñ Bongo", "Gómex", "La Concepción", "La Estella", "San Andrés", "Santa Marta", "Santa Rosa", "Santo Domingo", "Sortová", "Solano", "San Isidro"],
+        "David": ["Bijagual", "Cochea", "Chiriquí", "Guacá", "Las Lomas", "Pedregal", "San Carlos", "David", "David Este", "David Sur", "San Pablo Nuevo", "San Pablo Viejo"],
+        "Dolega": ["Dolega", "Dos Ríos", "Los Algarrobos", "Los Anastacios", "Potrerillos", "Proterillos Abajo", "Rovira", "Tinajas"],
+        "Gualaca": ["Gualaca", "Hornito", "Los Angeles", "Paja de Sombrero", "Rincón"],
+        "Remedios": ["El Nancito", "El Porvenir", "El Puerto", "Remedios", "Santa Lucía"],
+        "Renacimiento": ["Breñón", "Cañas Godas", "Dominical", "Monte Lirio", "Plaza de Caisán", "Río Sereno", "Santa Cruz", "Santa Clara"],
+        "San Félix": ["Las Lajas", "Lajas Adentro", "Juay", "San Félix", "Santa Cruz"],
+        "San Lorenzo": ["Boca Chica", "Boca del Monte", "Horconcitos", "San Juan", "San Lorenzo"],
+        "Tierras Altas": ["Volcán", "Cerro Punta", "Cuesta de Piedra", "Nueva California", "Paso Ancho"],
+        "Tolé": ["Bella Vista", "Cerro Viejo", "El Cristo", "Justo Fidel Palacios", "Lajas de Tolé", "Potrero de Caña", "Quebrada de Piedra", "Tolé", "Veladro"]
+    
+      },
+      "Darién": {
+        "Chepigana": ["Camogantí", "Chepigana", "Garachiné", "Jaqué", "La Palma", "Puerto Piña", "Sambú", "Setegantí", "Taimatí", "Tucutí"],
+        "Pinogana": ["Boca de Cupe", "El Real de Santa María", "Metetí", "Paya", "Pinogana", "Púcuro", "Yape", "Yaviza", "Wargandí"],
+        "Santa Fe": ["Agua Fría", "Cucunatí", "Río Congo", "Río Congo Arriba", "Río Iglesias", "Santa Fe", "Zapallal"]    
+      },
+      "Herrera": {
+        "Chitré": ["Chitré", "La Arena", "Llano Bonito", "San Juan Bautista", "Monagrillo"],
+        "Las Minas": ["Chepo", "Chumical", "El Toro", "Las Minas", "Leones", "Quebrada del Rosario", "Quebrada El Ciprián"],
+        "Los Pozos": ["El Capurí", "El Calabacito", "El Cedro", "La Arena", "La Pitaloza", "Las Llanas", "Los Cerritos", "Los Cerros de Paja", "Los Pozos"],
+        "Ocú": ["Cerro Largo", "El Tijera", "Entradero del Castillo", "Los Llanos", "Llano Grande", "Menchaca", "Peñas Chatas", "Ocú"],
+        "Parita": ["Cabuya", "Los Castillos", "Llano de la Cruz", "París", "Parita", "Portobelillo", "Potuga"],
+        "Pesé": ["El Barrero", "El Pedregoso", "El Ciruelo", "El Pájaro", "Las cabras", "Pesé", "Rincón Hondo", "Sabanagrande"],
+        "Santa María": ["Chupampa", "El Rincón", "El Limón", "Los Canelos", "Santa María"]
+     },
+     "Los Santos": {
+        "Guararé":["El Espinal", "El Hato", "El Macano", "Guararé", "Guararé Arriba", "La Enea", "La Pasera", "Las Trancas", "Llano Abajo", "Perales"],
+        "Las Tablas": ["Bajo Corral", "Bayano", "El Carate", "El Cocal", "El Manatial", "El Muñoz", "El Pedregoso", "El Sesteadero", "La Laja", "La Miel", "La Palma", "La Tiza", "Las Palmitas", "Las Tablas Abajo", "Nuario", "Palmira", "Peña Blanca", "Río Hondo", "San José", "San Miguel", "Las Tablas", "Santo Domingo", "Valle Rico", "Vallerriquito"],
+        "Los Santos": ["El Ejido", "El Guásimo", "La COlorada", "La Espigadilla", "La Villa de Los Santos", "Las Cruces", "Los Ángeles", "Los Olivos", "Llano Largo", "Sabanagrande", "Santa Ana", "Tres Quebradas", "Villa Lourdes", "Agua buena"],
+        "Macaracas": ["Bahía Honda", "Bajos de Güera", "Corozal", "Chupá", "El Cedro", "Espino Amarillo", "La Mesa", "Las Palmas", "Llano de Piedras", "Macaracas", "Mogollón"],
+        "Pedasí": ["Mariabé", "Oria Arriba", "Pedasí", "Purio"],
+        "Pocrí": ["El Cañafístulo", "Lajamina", "Paraíso", "Paritilla", "Pocrí"],
+        "Tonosí": ["Alto de Guera", "Cambutal", "Cañas", "El Bebedero", "El Cacao", "El Cortezo", "Flores", "Guánico", "Isla de Cañas", "La Tronosa", "Tonosí"]
+     },
+     "Panamá": {
+        "Balboa": ["La Ensenada", "La Esmeralda", "La Guinea", "Pedro González", "Saboga", "San Miguel"],
+        "Chepo": ["Cañita", "Chepillo", "El Llano", "Las Margaritas", "Chepo", "Santa Cruz de Chinina", "Tortí", "Madugandí"],
+        "Chimán": ["Brujas", "Chimán", "Gonzalo Vásquez", "Pásiga", "Unión Santeña"],
+        "Panamá": ["24 de Diciembre", "Alcalde Díaz", "Ancón", "Betania", "Bella Vista", "Calidonia", "Caimitillo", "Chilibre", "Curundú", "Don Bosco", "El Chorrillo", "Ernesto Córdoba Campos", "Juan Díaz", "Las Cumbres", "Las Garzas", "Las Mañanitas", "Pacora", "Parque Lefevre", "Pedregal", "Pueblo Nuevo", "Río Abajo", "San Felipe", "San Francisco", "San Martín", "Santa Ana", "Tocumen"],
+        "San Miguelito": ["Amalia Denis de Icaza", "Arnulfo Arias", "Belisario Frías", "Belisario Porras", "José Domingo Espinar", "Mateo Iturralde", "Omar Torrijos", "Rufina Alfaro", "Victoriano Lorenzo"],
+        "Taboga": ["Otoque Occidente", "Otoque Oriente", "Taboga"]
+     },
+     "Panamá Oeste": {
+        "Arraiján":["Arraiján", "Burunga", "Cerro Silvestre", "Juan Demóstenes Arosemena", "Nuevo Emperador", "Santa Clara", "Veracruz", "Vista Alegre"],
+        "Capira": ["Caimito", "Campana", "Capira", "Cermeño", "Cirí de Los Sotos", "El Cacao", "La Trinidad", "Las Ollas", "Lídice", "Villa Carmen", "Villa Rosario", "Santa Rosa"],
+        "Chame": ["Bejuco", "Buenos Aires", "Cabuya", "Chame", "Chicá", "El Líbano", "Las Lajas", "Nueva Gorgona", "Punta Chame", "Sajalices", "Sorá"],
+        "La Chorrera": ["Amador", "Arosemena", "Barrio Balboa", "Barrio Colón", "El Arado", "El Coco", "Feuillet", "Guadalupe", "Herrera", "Hurtado", "Iturralde", "La Represa", "Los Díaz", "Mendoza", "Obaldía", "Playa Leona", "Puerto Caimito", "Santa Rita"],
+        "San Carlos": ["El Espino", "El Higo", "Guayabito", "La Ermita", "La Laguna", "Las Uvas", "Los Llanitos", "San Carlos", "San José"]
+        },
+    "Veraguas": {
+        "Atalaya": ["Atalaya", "El Barrito", "La Carrillo", "La Montañuela", "San Antonio"],
+        "Calobre": ["Barnizal", "Calobre", "Chitra", "El Cocla", "El Potero", "La Laguna", "La Raya de Calobre", "La Tetilla", "La Yeguada", "Las Guías", "Monjarás", "San José"],
+        "Cañazas": ["Cañazas", "Cerro de Plata", "El Aromillo", "El Picador", "Las Cruces", "Los Valles", "San José", "San Marcelo"],
+        "La Mesa": ["Bisvalles", "Boró", "El Higo", "La Mesa", "Los Milagros", "Llano Grande", "San Bartolo"],
+        "Las Palmas": ["Cerro de Casa", "Corozal", "El María", "El Prado", "El Rincón", "Las Palmas", "Lol+a", "Manuel E. Amador Terrero", "Pixvae", "Puerto Vidal", "San Martín de Porres", "Viguí"],
+        "Mariato": ["Arenas", "El Cacao", "Mariato", "Quebro", "Tebario"],
+        "Montijo": ["Cébaco", "Costa Hermosa", "Gobernadora", "La Garceana", "Leones", "Montijo", "Pilón", "Unión del Norte"],
+        "Río de Jesús": ["Catorce de Noviembre", "Río de Jesús", "Las Huacas", "Los Castillos", "Utira"],
+        "San Francisco": ["Corral Falso", "Los Hatillos", "Remace", "San Francisco", "San Juan", "San José"],
+        "Santa Fe": ["Calovébora", "El Alto", "El Cuay", "El Pantano", "Gatuncito", "Río Luis", "Rubén Cantú", "Santa Fe"],
+        "Santiago": ["Canto del LlaNo", "Carlos Santana Ávila", "Edwin Fábrega", "La Colorada", "La Peña", "La Raya de Santa María", "Los Algarrobos", "Nuevo Santiago", "Ponuga", "San Martín de Porres", "San Pedro del Espino", "Santiago", "Santiago Este", "Santiago Sur", "Rodrigo Luque", "Urracá" ],
+        "Soná": ["Bahía Honda", "Calidonia", "Cativé", "El Marañón", "Guarumal", "Hicaco", "La Soledad", "La Trinchera", "Quebrada de Oro", "Río Grande", "Rodeo Viejo", "Soná"]
+    },
+    "Emberá-Wounaan": {
+        "Cémaco": ["Cirilo GUaynora", "Lajas Blancas", "Manuel ortega"],
+        "Sambú": ["Jingurudó", "Río Sabalo"],
+    },
+    "Guna Yala": {
+        "Ailigandí": [],
+        "Narganá": [],
+        "Puerto Obaldía": [],
+        "Tubualá": []
+    },
+    "Ngäbe-Buglé": {
+        "Besikó": ["Bocas de Balsa", "Cerro Banco", "Cerro Patena", "Camarón Arriba", "Emplanada de Chorcha", "Nämnoní", "Niba", "Soloy"],
+        "Kankintú": ["Bisira", "Calante", "Kankintú", "Guoroní", "Mününí", "Piedra Roja", "Tolote"],
+        "Kusapín": ["Bahía Azul", "Cañaveral", "Kusapín", "Río Chiriquí", "Tobobé"],
+        "Mironó": ["Cascabel", "Hato Corotú", "Hato Culantro", "Hato Pilón", "Jobo", "Hato Julí", "Quebrada de Loro", "Salto Dupí"],
+        "Müna": ["Alto Caballero", "Bakama", "Cerro Caña", "Cerro Puerco", "Chichica", "Krüa", "Maraca", "Nibra", "Peña Blanca", "Roka", "Sitio Prado", "Umaní", "Diko", "Kikari", "Dikeri", "Mreeni"],
+        "Nole Düima": ["Cerro Iglesias", "Hato Chamí", "Jädeberi", "Lajero", "Susama"],
+        "Ñürüm": ["Agua de Salud", "Alto de Jesús", "Buenos Aires", "Cerro Pelado", "El Bale", "El Paredón", "El Piro", "Guayabito", "Güibale", "El Peñon", "El Piro N°2"],
+        "Jirondai": ["Burí", "Guariviara", "Man Creek", "Samboa", "Tuwai"],
+        "Santa Catalina o Calovébora": ["Alto Bilingue", "Loma Yuca", "San Pedrito", "Valle Bonito", "Santa Catalina o Calovébora", ]
+        }
+    }
+    
+    window.onload = function() {
+      var provinciaSel = document.getElementById("provincia");
+      var distritoSel = document.getElementById("distrito");
+      var corregimientoSel = document.getElementById("corregimiento");
+      for (var x in provinciaObject) {
+        provinciaSel.options[provinciaSel.options.length] = new Option(x, x);
+      }
+      provinciaSel.onchange = function() {
+        //empty corregimientos- and distritos- dropdowns
+        corregimientoSel.length = 1;
+        distritoSel.length = 1;
+        //display correct values
+        for (var y in provinciaObject[this.value]) {
+          distritoSel.options[distritoSel.options.length] = new Option(y, y);
+        }
+      }
+      distritoSel.onchange = function() {
+        //empty Corregimientos dropdown
+        corregimientoSel.length = 1;
+        //display correct values
+        var z = provinciaObject[provinciaSel.value][this.value];
+        for (var i = 0; i < z.length; i++) {
+          corregimientoSel.options[corregimientoSel.options.length] = new Option(z[i], z[i]);
+        }
+      }
+    }
